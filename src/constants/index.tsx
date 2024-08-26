d65e5398f7cb6966eaa2931bf98049495c76381b
Zahra Mirkazemi <zahramirkazemi1997@gmail.com>
2024-08-26 21:08:17 +0330

    feat: Add app constants

---
+++ src/constants/index.tsx
@@ -0,0 +1,28 @@
+export const BASE_URL = "https://api.coingecko.com/api/v3/coins";
+
+export const tokenColumns = [
+  {
+    key: "image",
+    label: "Coin",
+  },
+  {
+    key: "name",
+    label: "Name",
+  },
+  {
+    key: "symbol",
+    label: "Symbol",
+  },
+  {
+    key: "current_price",
+    label: "Current Price",
+  },
+  {
+    key: "price_change_percentage_24h",
+    label: "24-hour Changes",
+  },
+  {
+    key: "total_volume",
+    label: "Trading Volume",
+  },
+];
